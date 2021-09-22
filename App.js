f6e5991ea2e8fd6981d08a41b85baf68b22c7804
Alex Rotar <alexrotar222@gmail.com>
2021-09-22 15:26:45 +0300

    make add and delete

--- App.js
+++ App.js
@@ -1,18 +1,53 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
 import Task from "./components/Task";
 
 export default function App() {
+    const [task, setTask] = useState('')
+    const [taskItem, setTaskItem] = useState([])
+
+    const taskHandler = (text) => {
+        if (text.length > 27) return
+        setTask(text)
+    }
+    const addTaskHandler = () => {
+        if (task.length < 1) return
+        setTaskItem([...taskItem, task])
+        setTask('')
+    }
+    const deleteTaskHandler = (index) => {
+        let itemsCopy = [...taskItem]
+        itemsCopy.splice(index, 1)
+        setTaskItem(itemsCopy)
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.tasksWrapper}>
                 <Text style={styles.sectionTitle}>Today task</Text>
+                {taskItem[0] ? null : <Text>you not have task</Text>}
                 <View style={styles.items}>
-                    <Task text={'task 1'}/>
-                    <Task text={'task 2'}/>
-                    <Task text={'task 3'}/>
+                    {taskItem.map((task, key) => {
+                        return (
+                            <TouchableOpacity key={key} onPress={() => deleteTaskHandler(key)}>
+                                <Task text={task}/>
+                            </TouchableOpacity>
+                        )
+                    })}
                 </View>
             </View>
+            <KeyboardAvoidingView
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                style={styles.writeTaskWrapper}
+            >
+                <TextInput style={styles.input} placeholder={'Write task'} value={task}
+                           onChangeText={text => taskHandler(text)}/>
+                <TouchableOpacity onPress={() => addTaskHandler()}>
+                    <View style={styles.addWrapper}>
+                        <Text style={styles.addText}>+</Text>
+                    </View>
+                </TouchableOpacity>
+            </KeyboardAvoidingView>
         </View>
     );
 }
@@ -32,5 +67,32 @@ const styles = StyleSheet.create({
     },
     items: {
         marginTop: 30
-    }
+    },
+    writeTaskWrapper: {
+        position: 'absolute',
+        bottom: 60,
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center'
+    },
+    input: {
+        padding: 15,
+        backgroundColor: '#FFF',
+        width: 250,
+        borderRadius: 60,
+        borderColor: '#C0C0C0',
+        borderWidth: 1
+    },
+    addWrapper: {
+        width: 60,
+        height: 60,
+        backgroundColor: '#FFF',
+        borderRadius: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: '#C0C0C0',
+        borderWidth: 1
+    },
+    addText: {}
 });
